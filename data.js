f0ec4c7cb748bdbfb5929605acfeb9efbf43a28d
Grant Patterson <darkrider231@gmail.com>
2022-01-19 13:25:03 -0500

    Simple Music Player in HTML/CSS/JS

---
+++ data.js
@@ -0,0 +1,35 @@
+let songs = [
+    {
+        name: 'Legendary',
+        path: 'musics/01 - Legendary.mp3',
+        artist: 'Skillet',
+        album: 'Unleashed',
+        date: 'Aug 05, 2016',
+        cover: 'images/skilletunleashed.png'
+    },
+    {
+        name: 'Back From the Dead',
+        path: 'musics/02 - Back From the Dead.mp3',
+        artist: 'Skillet',
+        album: 'Unleashed',
+        date: 'Aug 05, 2016',
+        cover: 'images/skilletunleashed.png'
+    },
+    {
+        name: 'Victorious',
+        path: 'musics/03 - Victorious.mp3',
+        artist: 'Skillet',
+        album: 'Victorious',
+        date: 'Aug 02, 2019',
+        cover: 'images/skilletvictorious.png'
+    },
+    {
+        name: 'Out of Hell',
+        path: 'musics/08 - Out of Hell.mp3',
+        artist: 'Skillet',
+        album: 'Unleashed',
+        date: 'Aug 05, 2016',
+        cover: 'images/skilletunleashed.png'
+    }
+    
+]
